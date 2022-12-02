a7cb13a7a6af53d9367bf1b4f5e9e75dda01dbc0
Yoel Villa <villayoel@gmail.com>
2022-12-02 20:07:52 +0100

    cambios

--- js/buscarReceta.js
+++ js/buscarReceta.js
@@ -33,11 +33,24 @@ document.write(``);
 document.write(`<table>`);
 document.write(`<td><h3>Comensales:</h3></td>`);
 //EJEMPLO ****
-document.write(`<td><input type="text" id="input1"></td>`);
-document.getElementById("input1").value =`${nombreReceta.comensales}`;
+document.write(`<td><input type="text" id="cambiarComensales"></td>`);
+document.getElementById("cambiarComensales").value =`${nombreReceta.comensales}`;
 //EJEMPLO *****
 document.write(`<tr><th>Ingrediente</th><th>Cantidad</th>`);
 for (i in nombreReceta.ingredientes){
-    document.write(`<tr><td>${nombreReceta.ingredientes[i].nombreIngrediente}</td><td>${nombreReceta.ingredientes[i].cantidad}</td></tr>`);
+    
+    document.write(`<tr><td><input type="text" id="cambiarIngredientes${i}"></td><td><input type="text" id = "cambiarCantIngredientes${i}"></td></tr>`);
+    document.getElementById(`cambiarIngredientes${i}`).value = `${nombreReceta.ingredientes[i].nombreIngrediente}`;
+    document.getElementById(`cambiarCantIngredientes${i}`).value = `${nombreReceta.ingredientes[i].cantidad}`;
 }
+document.write(`<tr><td>Añadir ingrediente :</td>`);
+document.write(`<td><input type="text" id="anadir-ingrediente" placeholder="Añadir ingrediente"/></td>`);
+document.write(`<td><input type="number" id="cantidad-ingrediente" placeholder="Cantidad"/><button type="button" id="btnAnadirIngrediente">+</button></td>`);
+
+document.write(`</tr>`);
+document.write(`<tr>`);
+document.write(`<td>Eliminar ingrediente :</td>`);
+document.write(`<td colspan ="2"><input type="text" id="eliminar-ingrediente" placeholder="Eliminar ingrediente"/><button type="button" id="btnEliminarIngrediente">-</button></td>`);
+document.write(`</tr>`);
+
 document.write(`</table>`);
